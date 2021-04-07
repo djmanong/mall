e66b9ae844d1c9977c8292db8ceece066e703c3f
djmanong <djmanong@qq.com>
2021-04-07 21:21:19 +0800

    项目结构创建

---
+++ mall-oms/src/test/java/com/djmanong/mall/oms/MallOmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djmanong.mall.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
